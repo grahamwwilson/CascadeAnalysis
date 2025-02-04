deee057f045ab07916f3c4408971cb80896f79aa
graham <Graham.Wilson@cern.ch>
2025-02-03 23:05:00 -0600

    Add simpler HPC version

---
+++ Test-HPC2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# This took about 7s on my laptop.
+
+# Target SM luminosity = 400 inv fb.
+# (the test LHE file here is actually the WWW SM background file that only has 10000 events)
+
+# The ROOT build 6.32.02 is in a conda environment. (It is called 6.32.2 on the system)
+
+module load root/6.32.2
+module list
+root --version
+python --version
+
+python analyze.py -h
+python analyze.py -w "Test" -t 400.0 -l 4.4241e3
+
+module unload root/6.32.2
+module list
+
+exit
