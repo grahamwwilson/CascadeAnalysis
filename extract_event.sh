dcee28f185b75b6f4d5ad3c658a9f433a4f16285
graham <Graham.Wilson@cern.ch>
2025-02-21 17:08:12 -0600

    try again

--- extract_event.sh
+++ extract_event.sh
@@ -10,6 +10,14 @@ lhe_file=$1
 event_number=$2
 
 # Extract the event using Perl to handle multi-line matching
-perl -00 -ne 'print if /<event>/../\/event>/ && $. == '$event_number'' "$lhe_file"
+#perl -00 -ne 'print if /<event>/../\/event>/ && $. == '$event_number'' "$lhe_file"
+
+# Find the start and end lines of the specified event, then extract it
+awk -v num=$event_number '
+    BEGIN {event_num=0}
+    /<event>/ {event_num++}
+    event_num == num {print}
+    /<\/event>/ && event_num == num {event_num=0}
+' "$lhe_file"
 
 exit
