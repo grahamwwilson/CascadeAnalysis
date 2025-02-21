cec3ef6d30e672bec7ac735eb75f1bb679b6aa38
graham <Graham.Wilson@cern.ch>
2025-02-21 17:05:59 -0600

    Now try Perl

--- extract_event.sh
+++ extract_event.sh
@@ -9,7 +9,7 @@ fi
 lhe_file=$1
 event_number=$2
 
-# Extract the event
-awk "/<event>/,/<\/event>/ {print}" "$lhe_file" | awk "NR==$event_number, NR==$event_number {print}"
+# Extract the event using Perl to handle multi-line matching
+perl -00 -ne 'print if /<event>/../\/event>/ && $. == '$event_number'' "$lhe_file"
 
 exit
