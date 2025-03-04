a514e48db892b84285b0d2982e772e653933d9f1
graham <Graham.Wilson@cern.ch>
2025-03-03 18:03:17 -0600

    Add had tau - > 3 l channels

---
+++ MakeSkimsOneHad.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# WZ -> l nu tau+ tau-
+#time ./StripAndWrite -i input60.lhe -o Run-60.lhe -c 2
+
+# ZZ -> l+l- tau+ tau- 
+time ./StripAndWrite -i input61.lhe -o Run-61-OneHad.lhe -c 1
+
+# ZZ -> tau+ tau- tau+ tau-
+time ./StripAndWrite -i input62.lhe -o Run-62-OneHad.lhe -c 3
+
+# WZ -> tau nu tau+ tau-
+#time ./StripAndWrite -i input63.lhe -o Run-63.lhe -c 3
+
+# WZ -> tau nu l+l-
+#time ./StripAndWrite -i input64.lhe -o Run-64.lhe -c 1
+
+exit
