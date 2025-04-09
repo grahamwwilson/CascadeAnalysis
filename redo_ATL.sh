59453be4a1440d24ea5ebf1e37eec7b1ed8fa680
graham <Graham.Wilson@cern.ch>
2025-04-09 11:01:22 -0500

    new model

---
+++ redo_ATL.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Target luminosity = 400 inv fb.
+
+# You can find the integrated luminosity of the Whizard run by searching for "corr. to luminosity" in the log file.
+# These can have some uncertainities due to the quality of the MC integration.
+
+# For these signal samples with identical parent slepton masses, these really should 
+# be exactly the same number.
+# I found, 2.2861e4, 2.2940e4, 2.2878e4. So let's use the average, namely, 2.2893e4
+
+ELMIN=$1
+MUMIN=$2
+
+echo "Using ELMIN: ${ELMIN} GeV for minimum pT for electrons"
+echo "Using MUMIN: ${MUMIN} GeV for minimum pT for muons" 
+
+module load root/6.32.2
+module list
+root --version
+python --version
+python analyze.py -h
+
+python analyze.py -w "ATLAS-250-150-100"  -t 400.0 -l 1.2032e3 -p "WF-V2" -e ${ELMIN} -m ${MUMIN}
+#python analyze.py -w "Sleptons-LSP220" -t 400.0 -l 2.2893e4 -p "WF-V1" -e ${ELMIN} -m ${MUMIN}
+#python analyze.py -w "Sleptons-LSP270" -t 400.0 -l 2.2893e4 -p "WF-V1" -e ${ELMIN} -m ${MUMIN}
+#python analyze.py -w "Sleptons-LSP260" -t 400.0 -l 2.2893e4 -p "WF-V1" -e ${ELMIN} -m ${MUMIN}
+
+module unload root/6.32.2
+module list
+
+exit
