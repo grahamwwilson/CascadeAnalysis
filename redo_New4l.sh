f218aaa950b44099f299ea4fddfc94deb905a62c
graham <Graham.Wilson@cern.ch>
2025-04-23 18:13:55 -0500

    Change order

--- redo_New4l.sh
+++ redo_New4l.sh
@@ -17,11 +17,12 @@ root --version
 python --version
 python analyze.py -h
 
+python analyze.py -w "ZDYll"  -t 400.0 -l 6.6213e-3 -p "." -e ${ELMIN} -m ${MUMIN}
+python analyze.py -w "ZDYtaultaul"  -t 400.0 -l 6.6213e-3 -p "." -e ${ELMIN} -m ${MUMIN}
 python analyze.py -w "WWlvlv"  -t 400.0 -l 2.7575e2 -p "WF-V2" -e ${ELMIN} -m ${MUMIN}
 python analyze.py -w "ZZllvv"  -t 400.0 -l 3.0904e3 -p "WF-V2" -e ${ELMIN} -m ${MUMIN}
 python analyze.py -w "ZZtau2l"  -t 400.0 -l 2.6543e3 -p "." -e ${ELMIN} -m ${MUMIN}
-python analyze.py -w "ZDYll"  -t 400.0 -l 6.6213e-3 -p "." -e ${ELMIN} -m ${MUMIN}
-python analyze.py -w "ZDYtaultaul"  -t 400.0 -l 6.6213e-3 -p "." -e ${ELMIN} -m ${MUMIN}
+
 
 module unload root/6.32.2
 module list
