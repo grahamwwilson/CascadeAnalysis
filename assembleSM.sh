9ace06d8a7445b45f4332d40f6298e52eb1edd4f
graham <Graham.Wilson@cern.ch>
2025-04-25 10:46:22 -0500

    Fix bug

--- assembleSM.sh
+++ assembleSM.sh
@@ -5,7 +5,7 @@
 
 module load root/6.32.2
 
-hadd histos_New4l.root histos_ZDYll.root histos_ZDYtaultaul.root histos_WWlvlv.root histos_ZZllvv.root histos_ZZtau2l.root WWtaultaul.root WWltaul.root
+hadd histos_New4l.root histos_ZDYll.root histos_ZDYtaultaul.root histos_WWlvlv.root histos_ZZllvv.root histos_ZZtau2l.root histos_WWtaultaul.root histos_WWltaul.root
 
 hadd histos_SM.root histos_New4l.root histos_WZ.root histos_ZZ.root histos_AllTaus.root histos_WWW.root histos_WWZ.root histos_WZZ.root histos_ZZZ.root histos_WZZMET.root histos_ZZZMET.root
 
