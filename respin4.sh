d5f7a8c36c78efd3365c285fc8b79c8f8aca994c
graham <Graham.Wilson@cern.ch>
2025-06-24 22:20:35 -0500

    fix bug

--- respin4.sh
+++ respin4.sh
@@ -3,7 +3,7 @@
 VERSION=$1
 echo 'Version '${VERSION}
 
-./respin.sh 10.0 10.0 HIPT-V$VERSION}
+./respin.sh 10.0 10.0 HIPT-V${VERSION}
 ./respin.sh 2.0 3.0 LOWESTPT-V${VERSION}
 ./respin.sh 5.0 5.0 LOWPT-V${VERSION}
 ./respin.sh 7.5 7.5 MEDPT-V${VERSION}
